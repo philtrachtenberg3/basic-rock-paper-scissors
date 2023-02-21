9b5b0e43b65736a6a2d865b8e61014be66211eaf
Philip Trachtenberg <philtrachtenberg@gmail.com>
2023-02-21 15:57:14 -0500

    some app done

---
+++ app.js
@@ -0,0 +1,33 @@
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let userChoice
+let computerChoice
+let result
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
+}))
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length) +1
+    if (randomNumber === 1) {
+        computerChoice = 'rock'
+    } 
+    if (randomNumber === 2) {
+        computerChoice = 'scissors'
+    } 
+    if (randomNumber === 3) {
+        computerChoice = 'paper'
+    } 
+    computerChoiceDisplay.innerHTML = computerChoice 
+}
+
+function getResult() {
+    if (computerChoice === userChoice) {
+        result = 'It is a draw!' 
+    }
+}
